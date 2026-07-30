a402f424e09c296b0e3b43fb7b63fa4a7040e66e
bowo-prasetyo <bpraz@yahoo.com>
2026-07-30 22:32:56 +0900

    Add files via upload

--- js/router/index.js
+++ js/router/index.js
@@ -1,1 +1,15 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.js';
+import EditorView from '../views/EditorView.js';
+import AboutView from '../views/AboutView.js';
 
+const routes = [
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/editor', name: 'editor', component: EditorView },
+    { path: '/about', name: 'about', component: AboutView }
+];
+
+export const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+});
