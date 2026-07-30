d80eadd14b1de558ee49bd3c857a01cb7857d1fe
bowo-prasetyo <bpraz@yahoo.com>
2026-07-30 22:34:40 +0900

    Add files via upload

--- js/components/BrickPalette.js
+++ js/components/BrickPalette.js
@@ -1,1 +1,8 @@
-
+export default {
+    name: 'BrickPalette',
+    template: `
+        <div class="brick-palette">
+            <p class="placeholder-text">Brick palette (coming in Step 5)</p>
+        </div>
+    `
+};
--- js/components/Sidebar.js
+++ js/components/Sidebar.js
@@ -1,1 +1,11 @@
+import BrickPalette from './BrickPalette.js';
 
+export default {
+    name: 'Sidebar',
+    components: { BrickPalette },
+    template: `
+        <aside class="sidebar">
+            <BrickPalette />
+        </aside>
+    `
+};
