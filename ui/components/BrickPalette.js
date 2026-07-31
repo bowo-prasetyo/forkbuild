db03e61044388e58e3c48a83559ff09d2371b636
bowo-prasetyo <bpraz@yahoo.com>
2026-07-31 16:03:19 +0900

    Add files via upload

---
+++ ui/components/BrickPalette.js
@@ -0,0 +1,8 @@
+export default {
+    name: 'BrickPalette',
+    template: `
+        <div class="brick-palette">
+            <p class="placeholder-text">Brick palette (coming in Step 5)</p>
+        </div>
+    `
+};
---
+++ ui/components/Toolbar.js
@@ -0,0 +1,9 @@
+export default {
+    name: 'Toolbar',
+    template: `
+        <div class="toolbar">
+            <span class="toolbar-title">Toolbar</span>
+            <span class="toolbar-hint">(tools land here in Step 6)</span>
+        </div>
+    `
+};
