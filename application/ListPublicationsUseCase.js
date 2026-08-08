8a7bac71872e190aa39bb76bd9c0b19e04a764ed
bowo-prasetyo <bpraz@yahoo.com>
2026-08-08 20:58:52 +0900

    Create ListPublicationsUseCase.js

---
+++ application/ListPublicationsUseCase.js
@@ -0,0 +1,18 @@
+// The single entry point for listing publications. Filters are optional
+// and additive: { author } or { parentDocumentId }. No filter returns
+// everything the provider knows about.
+export class ListPublicationsUseCase {
+    constructor(discoveryProvider) {
+        this._discoveryProvider = discoveryProvider;
+    }
+
+    execute(filter = {}) {
+        if (filter.author) {
+            return this._discoveryProvider.findByAuthor(filter.author);
+        }
+        if (filter.parentDocumentId) {
+            return this._discoveryProvider.findByParentId(filter.parentDocumentId);
+        }
+        return this._discoveryProvider.list();
+    }
+}
