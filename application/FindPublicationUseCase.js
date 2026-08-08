e106101c16e2b49656e9087d149c3889a3264cc5
bowo-prasetyo <bpraz@yahoo.com>
2026-08-08 20:59:25 +0900

    Create FindPublicationUseCase.js

---
+++ application/FindPublicationUseCase.js
@@ -0,0 +1,10 @@
+// The single entry point for retrieving one specific publication by id.
+export class FindPublicationUseCase {
+    constructor(discoveryProvider) {
+        this._discoveryProvider = discoveryProvider;
+    }
+
+    execute(id) {
+        return this._discoveryProvider.findById(id);
+    }
+}
