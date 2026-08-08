e318ae49ea43490a0dfaa4f4e890dd94c3129a6b
bowo-prasetyo <bpraz@yahoo.com>
2026-08-08 21:06:05 +0900

    Update index.js

--- ui/router/index.js
+++ ui/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/HomeView.js';
 import EditorView from '../views/EditorView.js';
+import RepositoryView from '../views/RepositoryView.js';
 import AboutView from '../views/AboutView.js';
 
 const routes = [
     { path: '/', name: 'home', component: HomeView },
     { path: '/editor', name: 'editor', component: EditorView },
+    { path: '/repository', name: 'repository', component: RepositoryView },
     { path: '/about', name: 'about', component: AboutView }
 ];
 
