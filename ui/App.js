6d004a1900699812ae0d95840d6044738af827b4
bowo-prasetyo <bpraz@yahoo.com>
2026-08-08 23:08:11 +0900

    Update App.js

--- ui/App.js
+++ ui/App.js
@@ -1,15 +1,21 @@
+import UserWidget from './components/UserWidget.js';
+
 export default {
     name: 'App',
+    components: { UserWidget },
     template: `
         <div class="app-shell">
             <header class="app-header">
                 <span class="app-title">ForkBuild</span>
-                <nav class="app-nav">
-                    <router-link to="/" class="app-nav-link">Home</router-link>
-                    <router-link to="/editor" class="app-nav-link">Editor</router-link>
-                    <router-link to="/repository" class="app-nav-link">Repository</router-link>
-                    <router-link to="/about" class="app-nav-link">About</router-link>
-                </nav>
+                <div class="app-header-right">
+	                <nav class="app-nav">
+	                    <router-link to="/" class="app-nav-link">Home</router-link>
+	                    <router-link to="/editor" class="app-nav-link">Editor</router-link>
+	                    <router-link to="/repository" class="app-nav-link">Repository</router-link>
+	                    <router-link to="/about" class="app-nav-link">About</router-link>
+	                </nav>
+                    <UserWidget />
+                </div>
             </header>
 
             <main class="app-content">
