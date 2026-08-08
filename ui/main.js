6e8fe4aff26556b994bdbc7e56f51f344b7a0897
bowo-prasetyo <bpraz@yahoo.com>
2026-08-08 23:06:37 +0900

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.js';
 import { router } from './router/index.js';
+import { CreateIdentityProviderUseCase } from '../application/CreateIdentityProviderUseCase.js';
+import { IdentityUseCase } from '../application/IdentityUseCase.js';
+
+const identityProvider = new CreateIdentityProviderUseCase().execute();
+const identityUseCase = new IdentityUseCase(identityProvider);
 
 const app = createApp(App);
-
+app.provide('identityUseCase', identityUseCase);
 app.use(router);
 app.mount('#app');
