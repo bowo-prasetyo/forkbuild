05d5fb85d694b5e90ea2d1f32771144557bf1251
bowo-prasetyo <bpraz@yahoo.com>
2026-08-09 09:52:13 +0900

    Update index.js

--- ui/router/index.js
+++ ui/router/index.js
@@ -3,11 +3,15 @@ import HomeView from '../views/HomeView.js';
 import EditorView from '../views/EditorView.js';
 import RepositoryView from '../views/RepositoryView.js';
 import AboutView from '../views/AboutView.js';
+import AuthorView from '../views/AuthorView.js';
+import WorldView from '../views/WorldView.js';
 
 const routes = [
     { path: '/', name: 'home', component: HomeView },
     { path: '/editor', name: 'editor', component: EditorView },
     { path: '/repository', name: 'repository', component: RepositoryView },
+    { path: '/author/:username', name: 'author', component: AuthorView },
+    { path: '/world/:documentId', name: 'world', component: WorldView },
     { path: '/about', name: 'about', component: AboutView }
 ];
 
