3173f5d50f5dfb2015c9ef178b9a1b2656a903e0
bowo-prasetyo <bpraz@yahoo.com>
2026-08-09 23:24:51 +0900

    Update BrickDefinition.js

--- core/BrickDefinition.js
+++ core/BrickDefinition.js
@@ -6,7 +6,10 @@ export class BrickDefinition {
         thumbnail = null,
         defaultRotation = 0,
         tags = [],
-        description = ''
+        description = '',
+        width = 1,
+        height = 1,
+        depth = 1
     }) {
         this._id = id;
         this._name = name;
@@ -15,33 +18,19 @@ export class BrickDefinition {
         this._defaultRotation = defaultRotation;
         this._tags = tags;
         this._description = description;
+        this._width = width;
+        this._height = height;
+        this._depth = depth;
     }
 
-    get id() {
-        return this._id;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get category() {
-        return this._category;
-    }
-
-    get thumbnail() {
-        return this._thumbnail;
-    }
-
-    get defaultRotation() {
-        return this._defaultRotation;
-    }
-
-    get tags() {
-        return this._tags;
-    }
-
-    get description() {
-        return this._description;
-    }
+    get id() { return this._id; }
+    get name() { return this._name; }
+    get category() { return this._category; }
+    get thumbnail() { return this._thumbnail; }
+    get defaultRotation() { return this._defaultRotation; }
+    get tags() { return this._tags; }
+    get description() { return this._description; }
+    get width() { return this._width; }
+    get height() { return this._height; }
+    get depth() { return this._depth; }
 }
