2284f9e9a9a6ca40d67304880f3a6fac0cd8856b
bowo-prasetyo <bpraz@yahoo.com>
2026-08-09 11:51:31 +0900

    Update CreateWorldViewUseCase.js

--- application/CreateWorldViewUseCase.js
+++ application/CreateWorldViewUseCase.js
@@ -1,15 +1,17 @@
 import { LocalStorageProvider } from '../storage/LocalStorageProvider.js';
 import { LoadPublicationDocumentUseCase } from './LoadPublicationDocumentUseCase.js';
 import { WorldViewSession } from './WorldViewSession.js';
+import { CreateWorldLayoutUseCase } from './CreateWorldLayoutUseCase.js';
 
 export class CreateWorldViewUseCase {
     execute() {
         const storageProvider = new LocalStorageProvider();
         const loadPublicationDocumentUseCase = new LoadPublicationDocumentUseCase(storageProvider);
+        const { worldLayoutProvider } = new CreateWorldLayoutUseCase().execute();
 
         return {
             createSession(registry) {
-                return new WorldViewSession({ registry, loadPublicationDocumentUseCase });
+                return new WorldViewSession({ registry, loadPublicationDocumentUseCase, worldLayoutProvider });
             }
         };
     }
