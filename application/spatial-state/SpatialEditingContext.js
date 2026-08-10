e0017e5d92c0c12816f1f2d5e29194340060f0bd
bowo-prasetyo <bpraz@yahoo.com>
2026-08-10 21:40:51 +0900

    Update SpatialEditingContext.js

--- application/spatial-state/SpatialEditingContext.js
+++ application/spatial-state/SpatialEditingContext.js
@@ -7,12 +7,14 @@ export class SpatialEditingContext {
         documentId = null,
         buildingId = null,
         brickId = null,
+        items = [],
         capabilities = {}
     } = {}) {
         this._type = type;
         this._documentId = documentId;
         this._buildingId = buildingId;
         this._brickId = brickId;
+        this._items = items.map((item) => ({ ...item }));
         this._capabilities = capabilities;
     }
 
@@ -30,6 +32,14 @@ export class SpatialEditingContext {
 
     get brickId() {
         return this._brickId;
+    }
+
+    get items() {
+        return this._items.map((item) => ({ ...item }));
+    }
+
+    get primary() {
+        return this._items.length > 0 ? { ...this._items[this._items.length - 1] } : null;
     }
 
     get capabilities() {
