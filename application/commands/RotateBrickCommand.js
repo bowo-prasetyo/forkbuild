2cc2192a1e6511d8a8bc4a8d1c88da17db3b62bd
bowo-prasetyo <bpraz@yahoo.com>
2026-08-10 10:48:15 +0900

    Update RotateBrickCommand.js

--- application/commands/RotateBrickCommand.js
+++ application/commands/RotateBrickCommand.js
@@ -1,1 +1,23 @@
+import { Command } from './Command.js';
 
+// Undoable rotation in degrees. The domain stores degrees; the renderer
+// converts to radians. deltaRotation is added to the current rotation.
+export class RotateBrickCommand extends Command {
+    constructor({ worldId, buildingId, brickId, deltaRotation }) {
+        super();
+        this._worldId = worldId;
+        this._buildingId = buildingId;
+        this._brickId = brickId;
+        this._deltaRotation = deltaRotation;
+        this._originalRotation = null;
+    }
+
+    get worldId() { return this._worldId; }
+    get buildingId() { return this._buildingId; }
+    get brickId() { return this._brickId; }
+
+    execute(context) {
+        this._assertWorldMatches(context);
+        const building = context.world.getBuilding(this._buildingId);
+        const brick = building ? building.findBrick(this._brickId) : null;
+        if
